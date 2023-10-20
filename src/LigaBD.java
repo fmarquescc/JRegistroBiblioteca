6ba5b013eb7d5daa7bd07683dec6aaf288cd3390
KalmeMarq <145478572+fmarquescc@users.noreply.github.com>
2023-10-20 15:06:40 +0100

    SQLLigaBD extends LigaBD

--- src/LigaBD.java
+++ src/LigaBD.java
@@ -5,80 +5,113 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public abstract class LigaBD {
+    public abstract void inserirLivro(Livro livro);
+    public abstract Livro buscarLivroPorTitulo(String titulo);
+    public abstract void atualizarLivro(Livro livro);
+    public abstract void excluirLivro(String titulo);
+    
     public static class LocalLigaBD {
+    }
+
+    public static class SqlLigaBD extends LigaBD{
         static String url = "jdbc:mysql://localhost:3306/utilizador?useTimezone=true&serverTimezone=UTC";
         static String user = "root";
         static String pass = "";
-    }
-
-    public static class SqlLigaBD {
-        public static Connection conectar() throws SQLException {
-            return DriverManager.getConnection(LocalLigaBD.url, LocalLigaBD.user, LocalLigaBD.pass);
+        
+        public Connection conectar() throws SQLException {
+            return DriverManager.getConnection(SqlLigaBD.url, SqlLigaBD.user, SqlLigaBD.pass);
         }
 
-        public static void inserirLivro(Livro livro) throws SQLException {
-            Connection connection = conectar();
-            String sql = "INSERT INTO livros (titulo, autor, editora, anolancamento) VALUES (?, ?, ?, ?)";
-            
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, livro.getTitulo());
-                preparedStatement.setString(2, livro.getAutor());
-                preparedStatement.setString(3, livro.getEditora());
-                preparedStatement.setString(4, livro.getAnolancamento());
-                
-                preparedStatement.executeUpdate();
-            } finally {
-                connection.close();
-            }
-        }
+        @Override
+        public void inserirLivro(Livro livro) {
+            try {
+                Connection connection = conectar();
+                String sql = "INSERT INTO livros (titulo, autor, editora, anolancamento) VALUES (?, ?, ?, ?)";
 
-        public static Livro buscarLivroPorTitulo(String titulo) throws SQLException {
-            Connection connection = conectar();
-            String sql = "SELECT * FROM livros WHERE titulo = ?";
-            
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, titulo);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                
-                if (resultSet.next()) {
-                    Livro livro = new Livro(resultSet.getString("titulo"),
-                            resultSet.getString("autor"),
-                            resultSet.getString("editora"),
-                            resultSet.getString("anolancamento"));
-                    return livro;
-                } else {
-                    return null;
+                try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                    preparedStatement.setString(1, livro.getTitulo());
+                    preparedStatement.setString(2, livro.getAutor());
+                    preparedStatement.setString(3, livro.getEditora());
+                    preparedStatement.setString(4, livro.getAnolancamento());
+
+                    preparedStatement.executeUpdate();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } finally {
+                    connection.close();
                 }
-            } finally {
-                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
 
-        public static void atualizarLivro(Livro livro) throws SQLException {
-            Connection connection = conectar();
-            String sql = "UPDATE livros SET autor = ?, editora = ?, anolancamento = ? WHERE titulo = ?";
-            
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, livro.getAutor());
-                preparedStatement.setString(2, livro.getEditora());
-                preparedStatement.setString(3, livro.getAnolancamento());
-                preparedStatement.setString(4, livro.getTitulo());
-                
-                preparedStatement.executeUpdate();
-            } finally {
-                connection.close();
+        @Override
+        public Livro buscarLivroPorTitulo(String titulo) {
+            try {
+                Connection connection = conectar();
+                String sql = "SELECT * FROM livros WHERE titulo = ?";
+
+                try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                    preparedStatement.setString(1, titulo);
+                    ResultSet resultSet = preparedStatement.executeQuery();
+
+                    if (resultSet.next()) {
+                        Livro livro = new Livro(resultSet.getString("titulo"),
+                                resultSet.getString("autor"),
+                                resultSet.getString("editora"),
+                                resultSet.getString("anolancamento"));
+                        return livro;
+                    } else {
+                        return null;
+                    }
+                } catch (SQLException e) {
+                        e.printStackTrace();
+                } finally {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        public void atualizarLivro(Livro livro) {
+            try {
+                Connection connection = conectar();
+                String sql = "UPDATE livros SET autor = ?, editora = ?, anolancamento = ? WHERE titulo = ?";
+
+                try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                    preparedStatement.setString(1, livro.getAutor());
+                    preparedStatement.setString(2, livro.getEditora());
+                    preparedStatement.setString(3, livro.getAnolancamento());
+                    preparedStatement.setString(4, livro.getTitulo());
+
+                    preparedStatement.executeUpdate();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } finally {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
 
-        public static void excluirLivro(String titulo) throws SQLException {
-            Connection connection = conectar();
-            String sql = "DELETE FROM livros WHERE titulo = ?";
-            
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, titulo);
-                preparedStatement.executeUpdate();
-            } finally {
-                connection.close();
+        @Override
+        public void excluirLivro(String titulo) {
+            try {
+                Connection connection = conectar();
+                String sql = "DELETE FROM livros WHERE titulo = ?";
+
+                try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                    preparedStatement.setString(1, titulo);
+                    preparedStatement.executeUpdate();
+                } finally {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
     }
