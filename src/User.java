02f1801ecb50109d1c734f2087d2b214b4e0ec3c
KalmeMarq <145478572+fmarquescc@users.noreply.github.com>
2023-10-20 14:51:16 +0100

    reset

--- src/User.java
+++
@@ -1,7 +0,0 @@
-public record User(String name, String email, String morada, String telefone, String nif, String login, String password) {
-    public String mostra() {
-        return "Nome: " + this.name + "\nEmail: " + this.email
-                + "\nMorada: " + this.morada + "\nTelefone: " + this.telefone
-                + "\nNIF: " + this.nif + "\nLogin: " + this.login + "\nPassword: " + this.password;
-    }
-}
