25b6224f6b8455c00a7c25869bdc209255a88722
Cristiano La Féria <138156003+crislaferia@users.noreply.github.com>
2023-10-20 14:55:14 +0100

    files francisco e cris

---
+++ src/Livros.java
@@ -0,0 +1,50 @@
+
+
+public class Livros {
+    private String titulo;
+    private String autor;
+    private String editora;
+    private String anolancamento;
+
+    public Livros(String titulo, String autor, String editora, String anolancamento) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editora = editora;
+        this.anolancamento = anolancamento;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getAnolancamento() {
+        return anolancamento;
+    }
+
+    public void setAnolancamento(String anolancamento) {
+        this.anolancamento = anolancamento;
+    }
+    
+    
+  
+}
