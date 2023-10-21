fed1af698d8f168acd55acd5ba7987866630ccc4
fmarquescc <145478572+fmarquescc@users.noreply.github.com>
2023-10-21 05:18:11 +0100

    finish LocalLigaBD

--- src/LigaBD.java
+++ src/LigaBD.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -5,7 +9,10 @@ import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 public abstract class LigaBD {
     public static LigaBD bd;
@@ -18,7 +25,7 @@ public abstract class LigaBD {
     }
     
     public abstract void inserirLivro(Livro livro);
-    public abstract Livro buscarLivroPorTitulo(String titulo);
+    public abstract Optional<Livro> buscarLivroPorTitulo(String titulo);
     public abstract List<Livro> obterLivros();
     public abstract void atualizarLivro(Livro livro);
     public abstract void excluirLivro(String titulo);
@@ -31,34 +38,152 @@ public abstract class LigaBD {
         
         @Override
         public void inserirLivro(Livro livro) {
+            List<Livro> list = this.obterLivros();
+            list.add(livro);
+            this.saveLivros(list);
         }
 
         @Override
-        public Livro buscarLivroPorTitulo(String titulo) {
-            return null;
+        public Optional<Livro> buscarLivroPorTitulo(String titulo) {
+            List<Livro> list = this.obterLivros();
+            Iterator<Livro> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                Livro livro = iterator.next();
+                if (livro.getTitulo().equals(titulo)) {
+                    return Optional.of(livro);
+                }
+            }
+            
+            return Optional.empty();
         }
 
         @Override
         public List<Livro> obterLivros() {
-            List<Livro> list = new ArrayList<>();
-            return list;
+            return this.loadLivros();
         }
 
         @Override
         public void atualizarLivro(Livro livro) {
+            List<Livro> list = this.obterLivros();
+            Iterator<Livro> iterator = list.iterator();
+        
+            while (iterator.hasNext()) {
+                Livro lv = iterator.next();
+                if (livro.getTitulo().equals(livro.getTitulo())) {
+                    lv.setAutor(livro.getAutor());
+                    lv.setEditora(livro.getEditora());
+                    lv.setAnolancamento(livro.getAnolancamento());
+                    break;
+                }
+            }
+            
+            this.saveLivros(list);
         }
 
         @Override
         public void excluirLivro(String titulo) {
+            List<Livro> list = this.obterLivros();
+            Iterator<Livro> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                Livro livro = iterator.next();
+                if (livro.getTitulo().equals(titulo)) {
+                    iterator.remove();
+                    break;
+                }
+            }
+            
+            this.saveLivros(list);
         }
 
         @Override
         public void inserirLeitor(Leitor leitor) {
+            List<Leitor> list = this.obterLeitors();
+            list.add(leitor);
+            this.saveLeitores(list);
         }
 
         @Override
-        public List<Leitor> obterLeitors() { 
-            return null;
+        public List<Leitor> obterLeitors() {
+            return this.loadLeitores();
+        }
+        
+        private void saveLivros(List<Livro> list) {
+            try (BufferedWriter writer = Files.newBufferedWriter(LIVROS_PATH)) {
+                for (int i = 0; i < list.size(); ++i) {
+                    Livro livro = list.get(i);
+                    writer.write(livro.getTitulo() + ';');
+                    writer.write(livro.getAutor() + ';');
+                    writer.write(livro.getEditora() + ';');
+                    writer.write(livro.getAnolancamento());
+                    
+                    if (i + 1 < list.size()) {
+                        writer.write('\n');
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        private void saveLeitores(List<Leitor> list) {
+            try (BufferedWriter writer = Files.newBufferedWriter(LEITORES_PATH)) {
+                for (int i = 0; i < list.size(); ++i) {
+                    Leitor leitor = list.get(i);
+                    writer.write(leitor.getNome() + ';');
+                    writer.write(leitor.getDatanas() + ';');
+                    writer.write(leitor.getNleitor() + ';');
+                    writer.write(leitor.getEmail() + ';');
+                    writer.write(leitor.getTelefone() + ';');
+                    writer.write(leitor.getLogin() + ';');
+                    writer.write(leitor.getPass());
+                    
+                    if (i + 1 < list.size()) {
+                        writer.write('\n');
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        private List<Livro> loadLivros() {
+            if (!Files.exists(LIVROS_PATH)) {
+                return Collections.emptyList();
+            }
+            
+            List<Livro> list = new ArrayList<>();
+            
+            try (BufferedReader reader = Files.newBufferedReader(LIVROS_PATH)) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] data = line.split(";");
+                    list.add(new Livro(data[0], data[1], data[2], data[3]));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            return list;
+        }
+        
+        private List<Leitor> loadLeitores() {
+            if (!Files.exists(LEITORES_PATH)) {
+                return Collections.emptyList();
+            }
+            
+            List<Leitor> list = new ArrayList<>();
+            
+            try (BufferedReader reader = Files.newBufferedReader(LEITORES_PATH)) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] data = line.split(";");
+                    list.add(new Leitor(data[0], data[1], data[2], data[3], data[4], data[5], data[6]));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            return list;
         }
     }
 
@@ -180,7 +305,7 @@ public abstract class LigaBD {
         }
 
         @Override
-        public Livro buscarLivroPorTitulo(String titulo) {
+        public Optional<Livro> buscarLivroPorTitulo(String titulo) {
             try {
                 Connection connection = conectar();
                 String sql = "SELECT * FROM livros WHERE titulo = ?";
@@ -194,9 +319,9 @@ public abstract class LigaBD {
                                 resultSet.getString("autor"),
                                 resultSet.getString("editora"),
                                 resultSet.getString("anolancamento"));
-                        return livro;
+                        return Optional.of(livro);
                     } else {
-                        return null;
+                        return Optional.empty();
                     }
                 } catch (SQLException e) {
                         e.printStackTrace();
@@ -206,7 +331,7 @@ public abstract class LigaBD {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-            return null;
+            return Optional.empty();
         }
 
         @Override
