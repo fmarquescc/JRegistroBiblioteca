a73eb670d6dc93952e0174f1028d93a61210b6a6
Cristiano La Féria <138156003+crislaferia@users.noreply.github.com>
2023-10-28 21:52:33 +0100

    experiencia do minimo da janela

--- src/dev/biblioteca/Biblioteca.java
+++ src/dev/biblioteca/Biblioteca.java
@@ -105,7 +105,7 @@ public class Biblioteca extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        setMinimumSize(new java.awt.Dimension(800, 600));
+        setMinimumSize(new java.awt.Dimension(600, 400));
 
         accountStatusLabel.setText("Sem Conta");
 
